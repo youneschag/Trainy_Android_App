726719b0573a8626ee406e8d4dd861bcdba77115
youneschaguer <chagueryounes@gmail.com>
2025-01-30 09:59:38 +0100

    Done version 7 (correct creating and editing exercises)

--- app/src/main/java/fr/uha/chaguer/trainy/ui/exercise/EditExerciseScreen.kt
+++ app/src/main/java/fr/uha/chaguer/trainy/ui/exercise/EditExerciseScreen.kt
@@ -1,8 +1,12 @@
 package fr.uha.chaguer.trainy.ui.exercise
 
-import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,13 +14,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import fr.uha.chaguer.android.ui.StateScreen
+import fr.uha.chaguer.android.ui.app.AppMenuEntry
+import fr.uha.chaguer.android.ui.app.AppTopBar
 import fr.uha.chaguer.trainy.R
 
 @Destination<RootGraph>
@@ -27,41 +33,34 @@ fun EditExerciseScreen(
     exerciseId: Long
 ) {
     val uiState by vm.uiState.collectAsStateWithLifecycle()
-    val context = LocalContext.current
-    var showErrorDialog by remember { mutableStateOf(false) }
+    val uiTitleState by vm.uiTitleState.collectAsStateWithLifecycle()
 
     LaunchedEffect(exerciseId) {
         vm.edit(exerciseId)
         vm.titleBuilder.setScreenNameId(R.string.edit_exercise)
     }
 
-    StateScreen(state = uiState) { content ->
-        SuccessExerciseScreen(
-            exercise = content,
-            send = { vm.send(it) },
-            onBackClick = {
-                if (content.nameState.value.isNullOrBlank() ||
-                    content.descriptionState.value.isNullOrBlank() ||
-                    (content.durationState.value ?: 0) <= 0 ||
-                    (content.repetitionsState.value ?: 0) <= 0
-                ) {
-                    showErrorDialog = true
-                } else {
-                    Toast.makeText(context, "Exercice modifié avec succès", Toast.LENGTH_SHORT).show()
-                    navigator.popBackStack()
-                }
-            }
+    val menuEntries = listOf(
+        AppMenuEntry.ActionEntry(
+            titleId = R.string.save,
+            icon = Icons.Filled.Save,
+            enabled = { uiTitleState.isSavable ?: false },
+            listener = { vm.save(); navigator.popBackStack() }
         )
-    }
+    )
 
-    if (showErrorDialog) {
-        AlertDialog(
-            onDismissRequest = { showErrorDialog = false },
-            confirmButton = {
-                Button(onClick = { showErrorDialog = false }) { Text("OK") }
-            },
-            title = { Text("Erreur") },
-            text = { Text("Veuillez remplir tous les champs avant d'enregistrer l'exercice") }
-        )
+    Scaffold(
+        topBar = {
+            AppTopBar(uiTitleState = uiTitleState, navigator, menuEntries = menuEntries)
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            StateScreen(state = uiState) { content ->
+                SuccessExerciseScreen(
+                    exercise = content,
+                    send = { event -> vm.send(event) }
+                )
+            }
+        }
     }
 }
--- app/src/main/java/fr/uha/chaguer/trainy/ui/exercise/SuccessExerciseScreen.kt
+++ app/src/main/java/fr/uha/chaguer/trainy/ui/exercise/SuccessExerciseScreen.kt
@@ -4,85 +4,50 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import fr.uha.chaguer.android.ui.field.OutlinedIntFieldWrapper
 import fr.uha.chaguer.android.ui.field.OutlinedTextFieldWrapper
 import fr.uha.chaguer.trainy.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SuccessExerciseScreen(
     exercise: ExerciseViewModel.UIState,
     send: (ExerciseViewModel.UIEvent) -> Unit,
-    onBackClick: () -> Unit
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Exercice", color = Color.White) },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        onBackClick()
-                    }) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = "Retour",
-                            tint = Color.White
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = Color(0xFF673AB7)
-                )
-            )
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .padding(16.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            OutlinedTextFieldWrapper(
-                field = exercise.nameState,
-                onValueChange = { send(ExerciseViewModel.UIEvent.NameChanged(it)) },
-                modifier = Modifier.fillMaxWidth(),
-                labelId = R.string.exercise_name,
-            )
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextFieldWrapper(
+            field = exercise.nameState,
+            onValueChange = { send(ExerciseViewModel.UIEvent.NameChanged(it)) },
+            modifier = Modifier.fillMaxWidth(),
+            labelId = R.string.exercise_name,
+        )
 
-            OutlinedTextFieldWrapper(
-                field = exercise.descriptionState,
-                onValueChange = { send(ExerciseViewModel.UIEvent.DescriptionChanged(it)) },
-                modifier = Modifier.fillMaxWidth(),
-                labelId = R.string.exercise_description,
-            )
+        OutlinedTextFieldWrapper(
+            field = exercise.descriptionState,
+            onValueChange = { send(ExerciseViewModel.UIEvent.DescriptionChanged(it)) },
+            modifier = Modifier.fillMaxWidth(),
+            labelId = R.string.exercise_description,
+        )
 
-            OutlinedIntFieldWrapper(
-                field = exercise.durationState,
-                onValueChange = { send(ExerciseViewModel.UIEvent.DurationChanged(it)) },
-                modifier = Modifier.fillMaxWidth(),
-                labelId = R.string.exercise_duration,
-            )
+        OutlinedIntFieldWrapper(
+            field = exercise.durationState,
+            onValueChange = { send(ExerciseViewModel.UIEvent.DurationChanged(it)) },
+            modifier = Modifier.fillMaxWidth(),
+            labelId = R.string.exercise_duration,
+        )
 
-            OutlinedIntFieldWrapper(
-                field = exercise.repetitionsState,
-                onValueChange = { send(ExerciseViewModel.UIEvent.RepetitionsChanged(it)) },
-                modifier = Modifier.fillMaxWidth(),
-                labelId = R.string.exercise_repetitions,
-            )
-        }
+        OutlinedIntFieldWrapper(
+            field = exercise.repetitionsState,
+            onValueChange = { send(ExerciseViewModel.UIEvent.RepetitionsChanged(it)) },
+            modifier = Modifier.fillMaxWidth(),
+            labelId = R.string.exercise_repetitions,
+        )
     }
 }
